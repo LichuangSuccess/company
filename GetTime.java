1f1c3c26d6412389e2c4659c76f46b7fd3def8ea
LichuangSuccess <56571902+LichuangSuccess@users.noreply.github.com>
2019-10-15 11:22:36 +0800

    Add files via upload
    时间工具类

---
+++ GetTime.java
@@ -0,0 +1,143 @@
+package com.fh.shop.gloaming.utils.time;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static com.sun.mail.imap.protocol.INTERNALDATE.format;
+
+/**
+ * @Effect TODO
+ * @Package com.fh.shop.gloaming.utils.time
+ * @Name GetTime
+ * @Author 幕夏夜微凉
+ * @Date 2019/10/10 19:41
+ **/
+public class GetTime {
+
+    /**
+     * 英文简写（默认）如：2010-12-01
+     */
+    public static String FORMAT_SHORT = "yyyy-MM-dd";
+
+    /**
+     * 英文全称  如：2010-12-01 23:15:06
+     */
+    public static String FORMAT_LONG = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 精确到毫秒的完整时间    如：yyyy-MM-dd HH:mm:ss.S
+     */
+    public static String FORMAT_FULL = "yyyy-MM-dd HH:mm:ss.S";
+
+    /**
+     * 中文简写  如：2010年12月01日
+     */
+    public static String FORMAT_SHORT_CN = "yyyy年MM月dd";
+
+    /**
+     * 中文全称  如：2010年12月01日  23时15分06秒
+     */
+    public static String FORMAT_LONG_CN = "yyyy年MM月dd日  HH时mm分ss秒";
+
+    /**
+     * 精确到毫秒的完整中文时间
+     */
+    public static String FORMAT_FULL_CN = "yyyy年MM月dd日  HH时mm分ss秒SSS毫秒";
+    private static Calendar calendar;
+
+    /**
+     * 获取时间戳
+     */
+    public static String getTimeString() {
+        SimpleDateFormat df = new SimpleDateFormat(FORMAT_FULL);
+        Calendar calendar = Calendar.getInstance();
+        return df.format(calendar.getTime());
+    }
+
+    /**
+     * 获取日期年份
+     * @param date 日期
+     * @return
+     */
+    public static String getYear(Date date) {
+        return format(date).substring(0, 4);
+    }
+    /**
+     * 功能描述：返回月
+     *
+     * @param date
+     *            Date 日期
+     * @return 返回月份
+     */
+    public static int getMonth(Date date) {
+        calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MONTH) + 1;
+    }
+
+    /**
+     * 功能描述：返回日
+     *
+     * @param date
+     *            Date 日期
+     * @return 返回日份
+     */
+    public static int getDay(Date date) {
+        calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    /**
+     * 功能描述：返回小
+     *
+     * @param date
+     *            日期
+     * @return 返回小时
+     */
+    public static int getHour(Date date) {
+        calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.HOUR_OF_DAY);
+    }
+
+    /**
+     * 功能描述：返回分
+     *
+     * @param date
+     *            日期
+     * @return 返回分钟
+     */
+    public static int getMinute(Date date) {
+        calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MINUTE);
+    }
+
+    /**
+     * 返回秒钟
+     *
+     * @param date
+     *            Date 日期
+     * @return 返回秒钟
+     */
+    public static int getSecond(Date date) {
+        calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.SECOND);
+    }
+
+    /**
+     * 功能描述：返回毫
+     *
+     * @param date
+     *            日期
+     * @return 返回毫
+     */
+    public static long getMillis(Date date) {
+        calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.getTimeInMillis();
+    }
+}
